b10a3c110b8480f450c1b620aebe58b753a76277
BogieBogard <mbogard@utexas.edu>
2018-09-07 15:30:45 -0500

    Added app.get and app.post for api/friends, fixed json syntax

--- server.js
+++ server.js
@@ -25,9 +25,10 @@ app.use(bodyParser.json());
 // =============================================================
 let profiles = [
   {
-    "name":"Elon Musk",
-    "photo":"https://amp.businessinsider.com/images/59f8d02cbcf93d46628b591c-750-500.jpg",
-    "scores":[
+    routeName: "elonmusk",
+    name:"Elon Musk",
+    photo:"https://amp.businessinsider.com/images/59f8d02cbcf93d46628b591c-750-500.jpg",
+    scores:[
         5,
         1,
         4,
@@ -41,9 +42,10 @@ let profiles = [
       ]
   },
   {
-    "name":"Mark Zuckerberg",
-    "photo":"https://cnet3.cbsistatic.com/img/pQWoXJ7cBNKcLeH-F4XYLdfqrIE=/970x0/2018/03/23/b2928724-8ca9-4aa6-92fe-fa97a42deb34/f8-facebook-mark-zuckerberg-0112.jpg",
-    "scores":[
+    routeName: "markzuckerberg",
+    name:"Mark Zuckerberg",
+    photo:"https://cnet3.cbsistatic.com/img/pQWoXJ7cBNKcLeH-F4XYLdfqrIE=/970x0/2018/03/23/b2928724-8ca9-4aa6-92fe-fa97a42deb34/f8-facebook-mark-zuckerberg-0112.jpg",
+    scores:[
         5,
         1,
         4,
@@ -57,9 +59,10 @@ let profiles = [
       ]
   },
   {
-    "name":"Sheryl Sandberg",
-    "photo":"https://specials-images.forbesimg.com/imageserve/5a8ef078a7ea431690141af5/416x416.jpg?background=000000&cropX1=1474&cropX2=3673&cropY1=407&cropY2=2605",
-    "scores":[
+    routeName: "sherylsandberg",
+    name:"Sheryl Sandberg",
+    photo:"https://specials-images.forbesimg.com/imageserve/5a8ef078a7ea431690141af5/416x416.jpg?background=000000&cropX1=1474&cropX2=3673&cropY1=407&cropY2=2605",
+    scores:[
         5,
         1,
         4,
@@ -81,6 +84,28 @@ console.log(profiles);
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+// Displays all profiles (possible matches)
+app.get("/api/friends", function(req, res) {
+  return res.json(profiles);
+});
+
+// Create new profiles - takes in JSON input
+app.post("/api/friends", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newProfile = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newProfile.routeName = newProfile.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newProfile);
+
+  friends.push(newProfile);
+
+  res.json(newProfile);
 });
 
 // Start our server so that it can begin listening to client requests.
